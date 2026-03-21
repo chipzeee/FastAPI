bdb5c050d98408204b3681136781e1f39152ac03
chipz <ajinkyadarekar528@gmail.com>
2026-03-21 13:29:41 +0530

    update and delete implemented

--- fastapi-telusko/main.py
+++ fastapi-telusko/main.py
@@ -17,3 +17,35 @@ products=[
 @app.get('/products')
 def get_all_products():
     return products
+
+@app.get('/products/{id}')
+def get_product_by_id(id : int):
+
+    for product in products:
+        if product.id==id:
+            return product
+    return "Product not found!!"
+
+@app.post('/products')
+def add_product(product:Products):
+    products.append(product)
+    return f"Product added. {product}"
+
+
+@app.put('/products')
+def update_product(id:int,product:Products):
+
+    for i in range(len(products)):
+        if products[i].id==id:
+            products[i]=product
+            return "Product updated successfully!"
+    return "Product with given id not found!!"
+
+
+@app.delete('/products')
+def delete_product(id:int):
+    for product in products:
+        if product.id==id:
+            products.remove(product)
+            return "Product deleted."
+    return "Product not found"
